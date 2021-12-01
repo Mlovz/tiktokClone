b2641f8663de2034c86720612beeb36b7898a39b
unknown <mestoev.yakhya@mail.ru>
2021-12-01 17:43:34 +0400

    tab proccesing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,8 @@ import Info from '../components/profile/Info'
 import {useSelector, useDispatch} from 'react-redux'
 import {useParams} from 'react-router-dom'
 import Button from '../components/Button'
+import Clips from '../components/Clips'
+import Likes from '../components/Likes'
 
 const Profile = () => {
     const {id} = useParams()
@@ -13,12 +15,16 @@ const Profile = () => {
     return (
         <div className='profile'>
             <Info auth={auth} dispatch={dispatch} id={id}/>
-            <div className="profile__tab d-flex align-items-center justify-content-between">
+            <div className="mt-4 profile__tab d-flex align-items-center justify-content-between">
                 <Button onClick={() => setTab(true)} fullWidth variant={tab && 'outlinedBottom'} colorText='#000'>Клипы</Button>
                 <Button onClick={() => setTab(false)} fullWidth variant={!tab && 'outlinedBottom'} colorText='#000'>Лайкнул(-а)</Button>
             </div>
-            
-           
+
+            {
+                tab
+                ? <Clips/>
+                : <Likes/>
+            }
             
         </div>
     )
