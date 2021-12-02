c13776095b9f116d8f2128c4d835e8baa06d872d
unknown <mestoev.yakhya@mail.ru>
2021-12-02 13:45:24 +0400

    add spinner

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Info from "../components/profile/Info";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import Button from "../components/Button";
 import Clips from "../components/Clips";
 import Likes from "../components/Likes";
 import { getProfileUser } from "../redux/actions/profileAction";
@@ -22,22 +21,21 @@ const Profile = () => {
   return (
     <div className="profile">
       {profile.loading ? (
-        <div className="text-dark d-flex align-items-center justify-content-center" style={{
-            fontSize: '25px'
-        }}>
-          <span
-            className="spinner-border spinner-border-md mx-2"
-            role="status"
-            aria-hidden="true"
-          ></span>
-          Loading...
+        <div className="spin">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       ) : (
         <Info auth={auth} profile={profile} dispatch={dispatch} id={id} />
       )}
       <div className="profile__tab">
-        <button onClick={() => setTab(false)} className={`${!tab && 'active'}`}>Клипы</button>
-        <button onClick={() => setTab(true)} className={`${tab && 'active'}`}>Лайкнул(-а)</button>
+        <button onClick={() => setTab(false)} className={`${!tab && "active"}`}>
+          Клипы
+        </button>
+        <button onClick={() => setTab(true)} className={`${tab && "active"}`}>
+          Лайкнул(-а)
+        </button>
       </div>
       {tab ? <Likes /> : <Clips />}
     </div>
