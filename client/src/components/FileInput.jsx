c5a8a42835726362902330a75fe98d794a2502df
unknown <mestoev.yakhya@mail.ru>
2021-12-02 15:43:38 +0400

    create ReactPlayer

--- client/src/components/FileInput.jsx
+++ client/src/components/FileInput.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Controller } from "react-hook-form";
 import DropZone from "react-dropzone";
 import Title from "./Title";
+import ReactPlayer from './ReactPlayer'
 
 const FileInput = ({ name, control }) => {
   return (
@@ -10,24 +11,44 @@ const FileInput = ({ name, control }) => {
       name={name}
       render={({ field: { onChange, onBlur, value } }) => {
         console.log(value);
-        return (
+        return value ? (
+          <div className="video__ctrl">
+            {
+              value[0].type.match(/video/i) &&
+              <ReactPlayer src={URL.createObjectURL(value[0])} width='100%' height='100%' volume={1} playing={true} />
+            }
+          </div>
+        ) : (
           <DropZone onDrop={onChange}>
             {({ getRootProps, getInputProps }) => (
               <div
+              className='drop__content'
                 style={{
                   padding: "2px",
                   height: "100%",
                 }}
                 {...getRootProps()}
               >
-                <i style={{fontSize: '30px', color:'#999999'}} className="far fa-cloud-upload-alt"></i>
-                <input {...getInputProps()} name={name} onBlur={onBlur} />
-                <Title fw={700} margin='15px 0 0 0'>Выберите видео для загрузки</Title>
+                <i
+                  style={{ fontSize: "30px", color: "#999999" }}
+                  className="far fa-cloud-upload-alt"
+                ></i>
+                <input
+                  {...getInputProps()}
+                  name={name}
+                  onBlur={onBlur}
+                  accept="video/mp4,video/x-m4v,video/*"
+                />
+                <Title fw={700} margin="15px 0 0 0">
+                  Выберите видео для загрузки
+                </Title>
                 <Title size={15}>или перетащите файл</Title>
-                <Title margin='15px 0 0 0' color='#999999'>MP4 или WebM</Title>
-                <Title color='#999999'>Разрешение 800x600 или выше</Title>
-                <Title color='#999999'>До 120 с</Title>
-                <Title color='#999999'>Менее 100mb</Title>
+                <Title margin="15px 0 0 0" color="#999999">
+                  MP4 или WebM
+                </Title>
+                <Title color="#999999">Разрешение 800x600 или выше</Title>
+                <Title color="#999999">До 120 с</Title>
+                <Title color="#999999">Менее 100mb</Title>
               </div>
             )}
           </DropZone>
---
+++ client/src/components/ReactPlayer.jsx
@@ -0,0 +1,44 @@
+import React, {  useState } from "react";
+import ReactPlayer from "react-player";
+
+const VideoSrc = ({ src, width, height }) => {
+  const [isVolume, setIsVolume] = useState(0);
+  const [isPlay, setIsPlay] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+
+  
+  
+  
+  return (
+    <div className="video__show h-100" onClick={() => setIsPlay(!isPlay)}>
+      <ReactPlayer
+        url={src}
+        width={width}
+        height={height}
+        playing={isPlay}
+        volume={isVolume}
+        
+        onProgress={(progress) => setSeconds(progress)}
+      />
+      <div className="video__show__pos video__show__volume" onClick={e => e.stopPropagation()}>
+          {
+              isVolume === 1 
+              ?<i onClick={() => setIsVolume(0)} className="far fa-volume-up"></i>
+              :<i onClick={() => setIsVolume(1)}  className="fal fa-volume-slash"></i>
+          }
+        
+      </div>
+
+      <div className="video__show__pos video__show__play">
+        {
+            !isPlay && <i onClick={(e) => {
+                setIsPlay(true)
+                e.stopPropagation()
+            }} className="fas fa-play"></i> 
+        }
+      </div>
+    </div>
+  );
+};
+
+export default VideoSrc;
