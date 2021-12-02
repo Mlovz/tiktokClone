1fbc9cddf1aeb6545775affeccf87dc31608f670
unknown <mestoev.yakhya@mail.ru>
2021-12-02 16:52:38 +0400

    create middleware(auth)

---
+++ controllers/postCtrl.js
@@ -0,0 +1,11 @@
+import Posts from '../models/postModel'
+
+
+const postCtrl = {
+    createPost: async(req, res) => {
+        const {title, video, isComment} = req.body
+    },
+}
+
+
+export default postCtrl
---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+import Users from "../models/userModel";
+import jwt from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+
+    if (!token)
+      return res.status(400).json({
+        msg: "Ошибка авторизации",
+      });
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decoded)
+      return res.status(400).json({
+        msg: "Ошибка авторизации",
+      });
+
+    const user = await Users.findOne({
+      _id: decoded.id,
+    });
+
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(500).json({
+      msg: err.message,
+    });
+  }
+};
+
+export default auth
