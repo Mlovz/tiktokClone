1d1e53de541215b6ba43c1c87320bd571b7972aa
unknown <mestoev.yakhya@mail.ru>
2021-12-03 14:14:49 +0400

    create route getPosts

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,5 +5,6 @@ import auth from '../middleware/auth'
 const router = express.Router()
 
 router.post('/create_post', auth, postCtrl.createPost)
+router.get('/get_auth_posts', auth, postCtrl.getPosts)
 
 export default router
